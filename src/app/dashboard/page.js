95486275f88efdabd1b8963f3b84e084f834ff64
D. Resnyanskiy <resnyanskiy05@gmail.com>
2025-05-04 03:17:13 +0300

    Added daisyui theme

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./dashboard.scss"
+
+function Page(props) {
+    return (
+        <div>
+            <button className="btn btn-primary">One</button>
+            <button className="btn btn-secondary">Two</button>
+            <button className="btn btn-accent btn-outline">Three</button>
+        </div>
+    );
+}
+
+export default Page;
